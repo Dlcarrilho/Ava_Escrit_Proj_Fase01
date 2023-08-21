541ab5e9ce7fd27c06190d912831d9543604ae34
Dlcarrilho <141149974+Dlcarrilho@users.noreply.github.com>
2023-08-21 13:36:50 +0000

    Calculando % de jogos_pay_forfree

--- main.py
+++ main.py
@@ -28,7 +28,16 @@ for a in range(0,20):
   if y not in amostragem: # garantindo que não escolha o mesmo elemento mais de uma vez
     amostragem.append(y)
 
-
+## CALCULANDO O % DE JOGOS GRATUITOS E PAGOS
+valor_maior_zero = []
+valor_menor_igual_zero = []
+for p in range(len(amostragem)):
+  if float(amostragem[p][7]) > 0:
+    valor_maior_zero.append(amostragem[p][7])
+  else:
+    valor_menor_igual_zero.append(amostragem[p][7])
+    
+    
 
   
 # Pergunta 1: Qual o percentual de jogos gratuitos e pagos na plataforma?
