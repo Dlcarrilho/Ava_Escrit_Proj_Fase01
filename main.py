fa0e4f2fd7ce44d6cb094b1fbd12ef0635ede62c
Dlcarrilho <141149974+Dlcarrilho@users.noreply.github.com>
2023-08-24 13:33:48 +0000

    Classes - Ger. Amostra + Leitura Arq. + Gravação Arq - FINALIZADAS

--- main.py
+++ main.py
@@ -27,32 +27,50 @@ class Lercsv():
 class Gravar():
   def __init__(self, arq):
     self.arq = arq
-
+ 
   def gravar(self):
     a = open('amostragem.csv', 'w', newline='')
     w = csv.writer(a)
     for i in self.arq:
       w.writerow(i)
 
-
 ## GERADOR DE AMOSTRAGEM
 
 class Amostra():
   def __init__(self, amostra):
     self.amostra = amostra
-    
+  
   def gerar_amostra(self):
     amostragem = []
     for a in range(0,20):
       y = random.choice(self.amostra) 
       if y not in amostragem: # garantindo que não escolha o mesmo elemento mais de uma vez
         amostragem.append(y)
-
     return amostragem
-
-
-
     
+## CALCULANDO O % DE JOGOS GRATUITOS E PAGOS
+
+class Estatistica():
+  def __init__(self, lista):
+        self.lista = lista
+  
+  def est_calc(self):
+    valor_maior_zero = []
+    valor_menor_igual_zero = []
+    for p in range(len(self.lista)):
+      
+      if float(self.lista[p][7]) > 0:
+        valor_maior_zero.append(self.lista[p][7])
+      else:
+        valor_menor_igual_zero.append(self.lista[p][7])
+    
+    tam_amostra = len(valor_maior_zero) + len(valor_menor_igual_zero)
+    per_gratuitos = round(float(len(valor_menor_igual_zero) / tam_amostra),3)
+    per_pagos =  round(1- float(len(valor_menor_igual_zero) / tam_amostra),3)
+    print(f'==> Percentual de Jogos Gratuitos: {per_gratuitos} | Percentual de jogos pagos: {per_pagos}  <==')
+
+## CALCULANDO: ANO COM MAIOR NUMERO DE JOGOS + LISTA DE COMO OS ANOS EMPATADOS
+
 # Pergunta 1: Qual o percentual de jogos gratuitos e pagos na plataforma?
 #Pergunta 2: Qual o ano com o maior número de novos jogos? Em caso de empate, retorne uma lista com os anos empatados.
 #Pergunta 3:  Para demonstrar a facilidade de revisão e modificação de uso do módulo desenvolvido, uma pergunta 
